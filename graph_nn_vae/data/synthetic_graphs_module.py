9c17f452a005c62af0617ff5adfb242dbd26c5eb
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2021-11-01 22:04:41 +0100

    refactor graph data module to allow for predefined graphs

--- graph_nn_vae/data/synthetic_graphs_module.py
+++ graph_nn_vae/data/synthetic_graphs_module.py
@@ -1,3 +1,4 @@
+from typing import List
 from argparse import ArgumentParser
 import networkx as nx
 import numpy as np
@@ -10,25 +11,35 @@ from graph_nn_vae.util import adjmatrix, split_dataset_train_val_test
 class SyntheticGraphsDataModule(BaseDataModule):
     data_name = "synthethic"
     pad_sequence = False
-    adjacency_matrices = []
 
-    def __init__(self, graph_type: str, num_dataset_graph_permutations: int, **kwargs):
+    def __init__(
+        self,
+        num_dataset_graph_permutations: int,
+        preprepared_graphs: List[nx.Graph] = None,
+        graph_type: str = "",
+        **kwargs
+    ):
         super().__init__(**kwargs)
         self.graph_type = graph_type
         self.num_dataset_graph_permutations = num_dataset_graph_permutations
-        self.data_name += "_" + graph_type
-        self.prepare_data()
+        if preprepared_graphs is None:
+            self.data_name += "_" + graph_type
+        else:
+            self.data_name = "preprepared"
+        self.prepare_data(preprepared_graphs=preprepared_graphs)
 
-    def prepare_data(self, *args, **kwargs):
+    def prepare_data(self, preprepared_graphs: List[nx.Graph] = None, *args, **kwargs):
         super().prepare_data(*args, **kwargs)
 
-        nx_graphs = create_synthetic_graphs(self.graph_type)
+        nx_graphs = preprepared_graphs
+        if nx_graphs is None:
+            nx_graphs = create_synthetic_graphs(self.graph_type)
         max_number_of_nodes = 0
         for graph in nx_graphs:
             if graph.number_of_nodes() > max_number_of_nodes:
                 max_number_of_nodes = graph.number_of_nodes()
 
-        self.adjacency_matrices = []
+        adjacency_matrices = []
         for nx_graph in nx_graphs:
             np_adj_matrix = nx.to_numpy_array(nx_graph, dtype=np.float32)
             for _ in range(self.num_dataset_graph_permutations):
@@ -36,13 +47,17 @@ class SyntheticGraphsDataModule(BaseDataModule):
                 reshaped_matrix = adjmatrix.minimize_and_pad(
                     adj_matrix, max_number_of_nodes
                 )
-                self.adjacency_matrices.append(reshaped_matrix)
+                adjacency_matrices.append(reshaped_matrix)
 
         (
             self.train_dataset,
             self.val_dataset,
             self.test_dataset,
-        ) = split_dataset_train_val_test(self.adjacency_matrices, [0.8, 0.1, 0.1])
+        ) = split_dataset_train_val_test(adjacency_matrices, [0.8, 0.1, 0.1])
+        if len(self.val_dataset) == 0 or len(self.train_dataset) == 0:
+            self.train_dataset = adjacency_matrices
+            self.val_dataset = adjacency_matrices
+            self.test_dataset = adjacency_matrices
 
     @staticmethod
     def add_model_specific_args(parent_parser: ArgumentParser):
@@ -57,7 +72,7 @@ class SyntheticGraphsDataModule(BaseDataModule):
         parser.add_argument(
             "--num_dataset_graph_permutations",
             dest="num_dataset_graph_permutations",
-            default=200,
+            default=10,
             type=int,
             help="number of permuted copies of the same graphs to generate in the dataset",
         )
--- graph_nn_vae/experiments/synthetic/recurrent_autoencoder_training.py
+++ graph_nn_vae/experiments/synthetic/recurrent_autoencoder_training.py
@@ -1,3 +1,5 @@
+import argparse
+import networkx as nx
 from argparse import ArgumentParser
 
 from graph_nn_vae.experiments.experiment import Experiment
@@ -13,12 +15,37 @@ class GraphAutoencoder(RecurrentGraphAutoencoder):
             loss_function="MSE",
             optimizer="Adam",
             embedding_size=256,
-            batch_size=8,
-            learning_rate=0.0001,
-            max_number_of_nodes=25,
+            batch_size=32,
+            learning_rate=0.00006,
+            gradient_clip_val=0.005,
+            max_number_of_nodes=20,
+            max_epochs=100000,
+            check_val_every_n_epoch=100,
+        )
+        return parser
+
+
+class PreparedGraphsDataModule(SyntheticGraphsDataModule):
+    @staticmethod
+    def add_model_specific_args(parent_parser: ArgumentParser):
+        parser = SyntheticGraphsDataModule.add_model_specific_args(parent_parser)
+        parser.add_argument(
+            "--preprepared_graphs",
+            dest="preprepared_graphs",
+            default=[
+                nx.grid_2d_graph(2, 2),
+                nx.grid_2d_graph(3, 2),
+                nx.grid_2d_graph(3, 3),
+                nx.grid_2d_graph(4, 3),
+                nx.grid_2d_graph(4, 4),
+            ],
+            help=argparse.SUPPRESS,
+        )
+        parser.set_defaults(
+            num_dataset_graph_permutations=4,
         )
         return parser
 
 
 if __name__ == "__main__":
-    Experiment(GraphAutoencoder, SyntheticGraphsDataModule).run()
+    Experiment(GraphAutoencoder, PreparedGraphsDataModule).run()
