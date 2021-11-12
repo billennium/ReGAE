d2dd3f9c04a6b54e065b43fdc5f7fcbc08be8ed8
Adam Małkowski <adam.malkowski@billennium.com>
2021-11-12 16:23:34 +0100

    Change dataset in experiment from Synethetic into Prepared

--- graph_nn_vae/experiments/synthetic/recurrent_autoencoder_training.py
+++ graph_nn_vae/experiments/synthetic/recurrent_autoencoder_training.py
@@ -56,4 +56,4 @@ class PreparedGraphsDataModule(SyntheticGraphsDataModule):
 
 
 if __name__ == "__main__":
-    Experiment(GraphAutoencoder, SyntheticGraphsDataModule).run()
+    Experiment(GraphAutoencoder, PreparedGraphsDataModule).run()
