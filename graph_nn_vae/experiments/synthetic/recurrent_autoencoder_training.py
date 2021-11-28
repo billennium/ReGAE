972aef3810c0d3c24f28e077249c9f2a3d991d85
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2021-11-28 01:02:27 +0100

    add roughly tuned autoencoder with mask parameters

--- graph_nn_vae/experiments/synthetic/recurrent_autoencoder_training.py
+++ graph_nn_vae/experiments/synthetic/recurrent_autoencoder_training.py
@@ -8,24 +8,32 @@ from graph_nn_vae.models.autoencoder_base import RecurrentGraphAutoencoder
 class GraphAutoencoder(RecurrentGraphAutoencoder):
     @staticmethod
     def add_model_specific_args(parent_parser: ArgumentParser):
+        # If using lr_schedulers that base their calculations on steps/epochs remember
+        # that the scheduling occurs at the frequency of the `check_val_every_n_epoch`
+        # interval. Thus, their calculations are skewed if it's higher than 1.
+        #
+        # To fix the intervals recalculate the values like this (MultiStepLR example):
+        # val_and_lr_update_interval = 20
+        # lr_milestones = [400, 800, 1200]
+        # lr_milestones = [v / val_and_lr_update_interval for v in lr_milestones]
+
         parser = RecurrentGraphAutoencoder.add_model_specific_args(parent_parser)
         parser.set_defaults(
             loss_function="BCEWithLogits",
             mask_loss_function="BCEWithLogits",
-            optimizer="Adam",
+            mask_loss_weight=0.5,
+            optimizer="AdamWAMSGrad",
             lr_monitor=True,
-            lr_scheduler_name="ReduceLROnPlateau",
-            lr_scheduler_params={"mode": "min", "verbose": True, "factor": 0.9},
+            lr_scheduler_name="NoSched",
             lr_scheduler_metric="loss/train_avg",
-            embedding_size=128,
+            learning_rate=0.001,
+            gradient_clip_val=1.0,
             batch_size=32,
-            learning_rate=0.0001,
-            max_number_of_nodes=16,
-            gradient_clip_val=None,
-            max_epochs=10000,
-            check_val_every_n_epoch=20,
+            embedding_size=128,
             encoder_hidden_layer_sizes=[1024],
+            encoder_activation_function="ELU",
             decoder_hidden_layer_sizes=[1024],
+            decoder_activation_function="ELU",
             metrics=[
                 "EdgeAccuracy",
                 "EdgePrecision",
@@ -33,6 +41,9 @@ class GraphAutoencoder(RecurrentGraphAutoencoder):
                 "MaskPrecision",
                 "MaskRecall",
             ],
+            max_number_of_nodes=16,
+            max_epochs=10000,
+            check_val_every_n_epoch=20,
             metric_update_interval=20,
             early_stopping=False,
             bfs=True,
