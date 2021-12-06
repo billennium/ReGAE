ef98bb3a9d7d36aedfabbdae0b2da9c979923c07
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2021-12-06 16:59:11 +0100

    move grid experiment to it's own dir

--- graph_nn_vae/experiments/synthetic_grid_small/recurrent_autoencoder_training.py
+++ graph_nn_vae/experiments/synthetic_grid_small/recurrent_autoencoder_training.py
@@ -3,11 +3,18 @@ from argparse import ArgumentParser
 from graph_nn_vae.experiments.experiment import Experiment
 from graph_nn_vae.data import SyntheticGraphsDataModule
 from graph_nn_vae.models.autoencoder_base import RecurrentGraphAutoencoder
-from graph_nn_vae.models.edge_decoders.memory_standard import MemoryEdgeDecoder
+from graph_nn_vae.models.autoencoder_components import (
+    BorderFillingGraphDecoder,
+    GraphDecoder,
+)
+from graph_nn_vae.models.edge_decoders.memory_standard import (
+    MemoryEdgeDecoder,
+    ZeroFillingMemoryEdgeDecoder,
+)
 from graph_nn_vae.models.edge_decoders.single_input_embedding import (
     MeanSingleInputMemoryEdgeDecoder,
     RandomSingleInputMemoryEdgeDecoder,
-    WeighingSingleInputMemoryEdgeDecoder,
+    WeightingSingleInputMemoryEdgeDecoder,
 )
 
 
@@ -23,9 +30,8 @@ class GraphAutoencoder(RecurrentGraphAutoencoder):
         # lr_milestones = [400, 800, 1200]
         # lr_milestones = [v / val_and_lr_update_interval for v in lr_milestones]
 
-        RecurrentGraphAutoencoder.edge_decoder_class = (
-            WeighingSingleInputMemoryEdgeDecoder
-        )
+        RecurrentGraphAutoencoder.graph_decoder_class = GraphDecoder
+        RecurrentGraphAutoencoder.edge_decoder_class = MemoryEdgeDecoder
 
         parser = RecurrentGraphAutoencoder.add_model_specific_args(parent_parser)
         parser.set_defaults(
