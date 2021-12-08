b9e42a66efca763d34e2651e1ca5aea613f57904
Adam Małkowski <adam.malkowski@billennium.com>
2021-12-08 21:48:16 +0100

    change default subgraph scheduler name

--- graph_nn_vae/data/smooth_learning_step_graph_data_module.py
+++ graph_nn_vae/data/smooth_learning_step_graph_data_module.py
@@ -157,7 +157,7 @@ class SmoothLearningStepGraphDataModule(SyntheticGraphsDataModule):
         parser.add_argument(
             "--subgraph_scheduler_name",
             dest="subgraph_scheduler_name",
-            default="uninform",
+            default="uniform",
             type=str,
             help="name of maximum subgraph size scheduler",
         )
