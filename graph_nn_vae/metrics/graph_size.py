a0b91eda2853506136a401e838334258c7faad37
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2021-12-14 19:13:15 +0100

    persist max graph size metric across epochs

--- graph_nn_vae/metrics/graph_size.py
+++ graph_nn_vae/metrics/graph_size.py
@@ -1,5 +1,6 @@
 import torch
 import torchmetrics
+from torchmetrics.utilities.imports import _LIGHTNING_AVAILABLE
 
 
 class MaxGraphSize(torchmetrics.MaxMetric):
@@ -15,6 +16,17 @@ class MaxGraphSize(torchmetrics.MaxMetric):
     ):
         max_num_nodes = torch.max(num_nodes)
         super().update(max_num_nodes)
+
+    def reset(self) -> None:
+        """Overloaded so that the metric persists between epochs."""
+        self._update_called = False
+        self._forward_cache = None
+        # lower lightning versions requires this implicitly to log metric objects correctly in self.log
+        if not _LIGHTNING_AVAILABLE or self._LIGHTNING_GREATER_EQUAL_1_3:
+            self._computed = None
+        # reset internal states
+        self._cache = None
+        self._is_synced = False
 
     @property
     def is_differentiable(self) -> bool:
