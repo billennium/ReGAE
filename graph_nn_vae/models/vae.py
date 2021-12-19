3eb9481a4e85c70257844cf84273a92d7f4a77ac
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2021-12-19 20:14:51 +0100

    add tuned grid_small VAE

--- graph_nn_vae/models/vae.py
+++ graph_nn_vae/models/vae.py
@@ -11,27 +11,19 @@ from graph_nn_vae.models.autoencoder_base import RecurrentGraphAutoencoder
 class RecurrentGraphVAE(RecurrentGraphAutoencoder):
     model_name = ""
 
-    def __init__(self, **kwargs):
+    def __init__(self, kld_loss_weight: float, **kwargs):
         super(RecurrentGraphVAE, self).__init__(**kwargs)
 
-        # latent_size = 1024
-        # self.nn_mu = nn.Linear(self.encoder.embedding_size, self.encoder.embedding_size)
         self.nn_log_var = nn.Linear(
             self.encoder.embedding_size, self.encoder.embedding_size
         )
-        # self.nn_log_var.bias.data.fill_(-5)
-        # self.nn_log_var.weight.data.fill_(0)
-        # self.nn_latent_to_embeddings = nn.Linear(
-        #     latent_size, self.encoder.embedding_size
-        # )
-        self.loss_kld_weight = 0.000000001
+        self.kld_loss_weight = kld_loss_weight
         # self.loss_kld_weight_step = 0.00001
 
     def step(self, batch, metrics: List[Callable] = []) -> Tensor:
         if not self.is_with_graph_mask:
             return super().step(batch, metrics)
 
-        # y_pred, diagonal_embeddings_norm = self(batch)
         y_pred, mu, log_var, diagonal_embeddings_norm = self(batch)
         y_edge, y_mask, y_pred_edge, y_pred_mask = self.adjust_y_to_prediction(
             batch, y_pred
@@ -43,14 +35,11 @@ class RecurrentGraphVAE(RecurrentGraphAutoencoder):
         loss_embeddings = (
             diagonal_embeddings_norm * self.diagonal_embeddings_loss_weight
         )
-
         loss_kld = self.calc_kld_loss(mu, log_var)
         # if self.trainer.current_epoch > 650:
-        self.loss_kld_weight = min(0.004, self.loss_kld_weight * 1.003)
+        #     self.loss_kld_weight = min(0.004, self.loss_kld_weight * 1.003)
 
-        self.log("loss_kld_weight", self.loss_kld_weight, on_step=False, on_epoch=True)
-
-        loss = loss_reconstruction + loss_embeddings + loss_kld * self.loss_kld_weight
+        loss = loss_reconstruction + loss_embeddings + loss_kld * self.kld_loss_weight
 
         for metric in metrics:
             metric(
@@ -63,6 +52,7 @@ class RecurrentGraphVAE(RecurrentGraphAutoencoder):
                 loss_embeddings=loss_embeddings,
                 loss_kld=loss_kld,
             )
+        self.log("loss_kld_weight", self.kld_loss_weight, on_step=False, on_epoch=True)
 
         return loss
 
@@ -72,16 +62,10 @@ class RecurrentGraphVAE(RecurrentGraphAutoencoder):
 
         raw_graph_embdeddings = self.encoder(batch)
 
-        # if self.trainer.current_epoch < 650:
-        #     graph_embeddings = raw_graph_embdeddings
-        #     log_var = None
-        # else:
         log_var = self.nn_log_var(raw_graph_embdeddings)
         graph_embeddings = self.reparameterize(
             mu=raw_graph_embdeddings, log_var=log_var
         )
-
-        # graph_embeddings = self.nn_latent_to_embeddings(latent_space_embeddings)
 
         reconstructed_graph_diagonals, diagonal_embeddings_norm = self.decoder(
             graph_encoding_batch=graph_embeddings,
@@ -94,7 +78,6 @@ class RecurrentGraphVAE(RecurrentGraphAutoencoder):
             log_var,
             diagonal_embeddings_norm,
         )
-        # return reconstructed_graph_diagonals, diagonal_embeddings_norm
 
     def distribute_latent_gaussian(self, x_hat: Tensor) -> Tuple[Tensor, Tensor]:
         return self.nn_mu(x_hat), self.nn_log_var(x_hat)
@@ -121,4 +104,12 @@ class RecurrentGraphVAE(RecurrentGraphAutoencoder):
     def add_model_specific_args(cls, parent_parser: ArgumentParser) -> ArgumentParser:
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
         parser = RecurrentGraphAutoencoder.add_model_specific_args(parent_parser=parser)
+        parser.add_argument(
+            "--kld_loss_weight",
+            dest="kld_loss_weight",
+            default=0.0001,
+            type=float,
+            metavar="LOSS_WEIGHT",
+            help="weight of the Kullback-Leibler divergence loss",
+        )
         return parser
