7698e4f741ce762c22124933dd3778145266740a
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2021-12-21 19:57:03 +0100

    add diagonal block to adj matrix conversion

---
+++ graph_nn_vae/data/diag_block_repr_graph_data_module.py
@@ -0,0 +1,68 @@
+from typing import List, Tuple
+
+import torch
+
+from graph_nn_vae.data.adj_matrix_data_module import AdjMatrixDataModule
+from graph_nn_vae.util.adjmatrix.diagonal_representation import (
+    adj_matrix_to_diagonal_representation,
+)
+
+
+class DiagonalBlockRepresentationGraphDataModule(AdjMatrixDataModule):
+    data_name = "DiagBlockRepr"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.collate_fn = collate_graph_batch
+
+    def _adj_batch_to_diagonal(
+        self, batch: List[Tuple[torch.Tensor, int]]
+    ) -> List[Tuple[torch.Tensor, torch.Tensor, int]]:
+
+        diag_represented_batch = []
+        for m in batch:
+            matrix = m[0]
+            num_nodes = m[1]
+            diag_represented_matrix = adj_matrix_to_diagonal_representation(
+                matrix, num_nodes
+            )
+            graph_mask = torch.ones(diag_represented_matrix.shape)
+            diag_represented_batch.append(
+                (diag_represented_matrix, graph_mask, num_nodes)
+            )
+
+        return diag_represented_batch
+
+    def prepare_data(self, *args, **kwargs):
+        super().prepare_data(*args, **kwargs)
+        self.train_dataset = self._adj_batch_to_diagonal(self.train_dataset)
+        self.val_dataset = self._adj_batch_to_diagonal(self.val_dataset)
+        self.test_dataset = self._adj_batch_to_diagonal(self.test_dataset)
+
+    def train_dataloader(self, **kwargs):
+        dl = super().train_dataloader(**kwargs)
+        dl.collate_fn = self.collate_fn
+        return dl
+
+    def val_dataloader(self, **kwargs):
+        dl = super().val_dataloader(**kwargs)
+        dl.collate_fn = self.collate_fn
+        return dl
+
+    def test_dataloader(self, **kwargs):
+        dl = super().test_dataloader(**kwargs)
+        dl.collate_fn = self.collate_fn
+        return dl
+
+
+def collate_graph_batch(batch):
+    # As part of the collation graph diag_repr are padded with 0.0 and the graph masks
+    # are padded with 1.0 to represent the end of the graphs.
+    graphs = torch.nn.utils.rnn.pad_sequence(
+        [g[0] for g in batch], batch_first=True, padding_value=0.0
+    )
+    graph_masks = torch.nn.utils.rnn.pad_sequence(
+        [g[1] for g in batch], batch_first=True, padding_value=0.0
+    )
+    num_nodes = torch.tensor([g[2] for g in batch])
+    return graphs, graph_masks, num_nodes
--- graph_nn_vae/util/adjmatrix/diagonal_block_representation.py
+++ graph_nn_vae/util/adjmatrix/diagonal_block_representation.py
@@ -74,3 +74,87 @@ def adj_matrix_to_diagonal_block_representation(
     concatenated_diagonals = torch.cat(diagonals, dim=0)
 
     return concatenated_diagonals
+
+
+def diagonal_block_to_adj_matrix_representation(
+    diagonal_block_graph: torch.Tensor, num_nodes: int
+) -> torch.Tensor:
+    """
+    The diagonal block representatuin has a shape like [ block_idx  : edge_y_idx : edge_x_idx : edge      ]
+                                                       [ num_blocks : block_size : block_size : edge_size ]
+
+    For a block size of 4 a diagonal representation like:
+
+    1 0  0 0  0 0
+    0 1  1 0  1 0
+
+    Translates to:
+     x 0 1 2 3
+    y
+    0  0 0 0 0
+    1  1 0 0 0
+    2  1 0 0 0
+    3  0 1 1 0
+
+    to a shape [y, x, edge_size]
+    """
+
+    num_blocks = diagonal_block_graph.shape[0]
+    block_size = diagonal_block_graph.shape[1]
+    edge_size = diagonal_block_graph.shape[-1]
+    num_columns = divide_integer_round_up(num_nodes - 1, block_size)
+
+    num_missing_blocks_for_full_matrix = num_columns * num_columns - num_blocks
+    diagonal_block_graph = torch.nn.functional.pad(
+        diagonal_block_graph, (0, 0, 0, 0, 0, 0, 0, num_missing_blocks_for_full_matrix)
+    )
+
+    # create indices mapping the block diagonal to columns of a matrix
+    columns = [[] for _ in range(num_columns)]
+    num_blocks_in_diagonal = 1
+    diagonal_offset = 0
+    max_idx = 0
+    while diagonal_offset < num_blocks:
+        for idx_in_diagonal in range(num_blocks_in_diagonal):
+            max_idx = diagonal_offset + idx_in_diagonal
+            columns[idx_in_diagonal].append(max_idx)
+        diagonal_offset += num_blocks_in_diagonal
+        num_blocks_in_diagonal += 1
+
+    for i in range(num_columns):
+        columns[i].extend([max_idx + 1 for _ in range(i)])
+
+    bottom_up_column_indices = torch.tensor(columns)
+    bottom_up_column_indices = torch.rot90(bottom_up_column_indices)
+    flattened_indices = bottom_up_column_indices.flatten()
+
+    # recreate a "blocky" adjacency matrix
+    flattened_rows_graph = diagonal_block_graph[flattened_indices]
+    block_adj_matrix = flattened_rows_graph.view(
+        num_columns,
+        num_columns,
+        block_size,
+        block_size,
+        edge_size,
+    )
+
+    # concatenate the blocks into a flatter matrix
+    block_rows = [row[0] for row in torch.split(block_adj_matrix, 1, dim=0)]
+    columnar_block_adj_matrix = torch.cat(block_rows, dim=1)
+    columns = [column[0] for column in torch.split(columnar_block_adj_matrix, 1, dim=0)]
+    adj_matrix = torch.cat(columns, dim=1)
+
+    # adjust the shape
+    pad_diff = num_nodes - adj_matrix.shape[0]
+    if pad_diff > 0:
+        adj_matrix = torch.nn.functional.pad(
+            adj_matrix, (0, 0, 0, pad_diff, pad_diff, 0)
+        )
+    elif pad_diff < 0:
+        adj_matrix = adj_matrix[-pad_diff:, :pad_diff, :]
+
+    return adj_matrix
+
+
+def divide_integer_round_up(dividend, divisor) -> int:
+    return int((dividend + divisor - 1) / divisor)
