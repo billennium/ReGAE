c31a35a39784ea2e3771f43e1a44fdd8939306e6
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2021-12-22 10:30:25 +0100

    add diagonal_block_to_adj_matrix tests

--- tests/util/adjmatrix/test_diagonal_block_representation.py
+++ tests/util/adjmatrix/test_diagonal_block_representation.py
@@ -3,6 +3,7 @@ import pytest
 import torch
 from graph_nn_vae.util.adjmatrix.diagonal_block_representation import (
     adj_matrix_to_diagonal_block_representation,
+    diagonal_block_to_adj_matrix_representation,
 )
 
 
@@ -100,4 +101,108 @@ def test_adj_matrix_to_diagonal_block_representation(
     output = adj_matrix_to_diagonal_block_representation(
         input_matrix, num_nodes, block_size
     )
+    assert torch.equal(output, expected)
+
+
+@pytest.mark.parametrize(
+    "diagonal_block,num_nodes,expected",
+    [
+        # fmt: off
+        (
+            [[[1]]],
+            2,
+            [[0, 0],
+             [1, 0]],
+        ),
+        (
+            [[[0, 0],
+              [1, 0]]],
+            2,
+            [[0, 0],
+             [1, 0]],
+        ),
+        (
+            [[[0, 0, 0],
+              [0, 0, 0],
+              [1, 0, 0]]],
+            2,
+            [[0, 0],
+             [1, 0]],
+        ),
+        (
+            [[[0, 0, 0],
+              [0, 0, 0],
+              [1, 1, 0]]],
+            3,
+            [[0, 0, 0],
+             [0, 0, 0],
+             [1, 1, 0]],
+        ),
+        (
+            [[[0, 0],
+              [1, 1]]],
+            3,
+            [[0, 0, 0],
+             [0, 0, 0],
+             [1, 1, 0]],
+        ),
+        (
+            [[[1, 0],
+              [0, 1]],
+             [[0, 0],
+              [1, 0]],
+             [[0, 0],
+              [1, 0]]],
+            4,
+            [[0, 0, 0, 0],
+             [1, 0, 0, 0],
+             [1, 0, 0, 0],
+             [0, 1, 1, 0]],
+        ),
+        (
+            [[[1, 0, 0],
+              [1, 0, 0],
+              [0, 1, 1]]],
+            4,
+            [[0, 0, 0, 0],
+             [1, 0, 0, 0],
+             [1, 0, 0, 0],
+             [0, 1, 1, 0]],
+        ),
+        (
+            [[[0]], [[1]], [[1]], [[1]], [[0]], [[1]]],
+            4,
+            [[0, 0, 0, 0],
+             [1, 0, 0, 0],
+             [1, 0, 0, 0],
+             [0, 1, 1, 0]],
+        ),
+        (
+            [[[1, 0, 0],
+              [1, 0, 0],
+              [0, 1, 1]],
+             [[0, 0, 0],
+              [0, 0, 0],
+              [1, 0, 0]],
+             [[0, 0, 0],
+              [0, 0, 0],
+              [1, 0, 0]]],
+            5,
+            [[0, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [0, 1, 1, 1, 0]],
+        ),
+        # fmt: on
+    ],
+)
+def test_to_diagonal_block_to_adj_matrix_representation(
+    diagonal_block, num_nodes, expected
+):
+    input_diagonal = torch.Tensor(diagonal_block)
+    input_diagonal = input_diagonal[:, :, :, None]
+    expected = torch.Tensor(expected)
+    expected = expected[:, :, None]
+    output = diagonal_block_to_adj_matrix_representation(input_diagonal, num_nodes)
     assert torch.equal(output, expected)
