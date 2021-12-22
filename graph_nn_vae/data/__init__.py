4215e9e1aa0027f0640fd5e491672fd46356c5a7
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2021-12-22 12:41:34 +0100

    add missing data module init declaration

--- graph_nn_vae/data/__init__.py
+++ graph_nn_vae/data/__init__.py
@@ -1,5 +1,8 @@
 from .data_module import BaseDataModule
 from .graph_loaders import GraphLoaderBase, SyntheticGraphLoader, RealGraphLoader
 from .diag_repr_graph_data_module import DiagonalRepresentationGraphDataModule
+from .diag_block_repr_graph_data_module import (
+    DiagonalBlockRepresentationGraphDataModule,
+)
 from .smooth_learning_step_graph_data_module import SmoothLearningStepGraphDataModule
 from graph_nn_vae.data.graph_loaders import GraphLoaderBase
