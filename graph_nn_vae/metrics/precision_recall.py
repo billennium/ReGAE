dd84a0340f6b91d48df52b1285b7b2834bbceb14
Adam Małkowski <adam.malkowski@billennium.com>
2022-01-06 14:37:54 +0100

    add weighted versions of precision and recall

--- graph_nn_vae/metrics/precision_recall.py
+++ graph_nn_vae/metrics/precision_recall.py
@@ -1,47 +1,99 @@
 import torch
 import torchmetrics
 
+from graph_nn_vae.util.adjmatrix.diagonal_block_representation import (
+    calculate_num_blocks,
+)
 
-class EdgePrecision(torchmetrics.Precision):
+
+def torch_bincount(t):
+    """
+    torch.bincount() when used on CUDA may lead to nondeterministic gradients. From testing, this isn't an issue in our use case.
+    """
+    was_deterministic = torch.are_deterministic_algorithms_enabled()
+    torch.use_deterministic_algorithms(False)
+    t = torch.bincount(t)
+    torch.use_deterministic_algorithms(was_deterministic)
+    return t
+
+
+class EdgeMetric(torchmetrics.Metric):
+    label = "edge_metric"
+    metric_class = None
+    weight_power = 1
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.graphs_metrics = {}
+        # self.num_graphs = {}
+        self.metric = self.metric_class(num_classes=2, average=None, **kwargs)
+        self.metrics = []
+        self.weights = []
+
+    def update(
+        self,
+        edges_predicted: torch.Tensor,
+        edges_target: torch.Tensor,
+        num_nodes: torch.Tensor,
+        **kwargs,
+    ):
+        block_size = edges_predicted.shape[2]
+
+        edges_predicted = torch.sigmoid(edges_predicted).round().int()
+        edges_target = torch.clamp(edges_target.int(), min=0)
+
+        num_blocks = calculate_num_blocks(num_nodes, block_size)
+        graph_counts_per_size = torch_bincount(num_blocks)
+
+        for index, count in enumerate(graph_counts_per_size):
+            if count:
+                mask = num_blocks == index
+                predicted = edges_predicted[mask].flatten()
+                target = edges_target[mask].flatten()
+
+                self.metrics.append(self.metric(predicted, target)[1])
+
+                self.weights.append(count / pow(index * block_size, self.weight_power))
+
+    def compute(self) -> torch.Tensor:
+        metrics = torch.stack(self.metrics)
+        weights = torch.stack(self.weights)
+
+        return (metrics * weights).sum() / weights.sum()
+
+
+class EdgePrecision(EdgeMetric):
     label = "edge_precision"
-
-    def __init__(self, **kwargs):
-        super().__init__(num_classes=2, average=None, **kwargs)
-
-    def update(
-        self,
-        edges_predicted: torch.Tensor,
-        edges_target: torch.Tensor,
-        **kwargs,
-    ):
-        edges_predicted = torch.sigmoid(edges_predicted).round().int().flatten()
-        edges_target = torch.clamp(edges_target.int().flatten(), min=0)
-
-        super().update(edges_predicted, edges_target)
-
-    def compute(self) -> torch.Tensor:
-        return super().compute()[1]
+    metric_class = torchmetrics.Precision
 
 
-class EdgeRecall(torchmetrics.Recall):
+class EdgeRecall(EdgePrecision):
     label = "edge_recall"
+    metric_class = torchmetrics.Recall
 
-    def __init__(self, **kwargs):
-        super().__init__(num_classes=2, average=None, **kwargs)
 
-    def update(
-        self,
-        edges_predicted: torch.Tensor,
-        edges_target: torch.Tensor,
-        **kwargs,
-    ):
-        edges_predicted = torch.sigmoid(edges_predicted).round().int().flatten()
-        edges_target = torch.clamp(edges_target.int().flatten(), min=0)
+class EdgePrecisionNonWeighted(EdgeMetric):
+    weight_power = 0
+    label = "edge_precision_non_weighted"
+    metric_class = torchmetrics.Precision
 
-        super().update(edges_predicted, edges_target)
 
-    def compute(self) -> torch.Tensor:
-        return super().compute()[1]
+class EdgeRecallNonWeighted(EdgePrecision):
+    weight_power = 0
+    label = "edge_recall_non_weighted"
+    metric_class = torchmetrics.Recall
+
+
+class EdgePrecisionSquareWeighted(EdgeMetric):
+    weight_power = 2
+    label = "edge_precision_square_weighted"
+    metric_class = torchmetrics.Precision
+
+
+class EdgeRecallSquareWeighted(EdgePrecision):
+    weight_power = 2
+    label = "edge_recall_square_weighted"
+    metric_class = torchmetrics.Recall
 
 
 class MaskPrecision(torchmetrics.Precision):
