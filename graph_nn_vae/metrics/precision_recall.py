fix precision and recall calculation - reset state

graph_nn_vae/metrics/precision_recall.py
@@ -15,8 +15,9 @@ class EdgeMetric(torchmetrics.Metric):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.metric = self.metric_class(num_classes=2, average=None, **kwargs)
-        self.metrics = []
-        self.weights = []
+
+        self.add_state("metrics", default=[], dist_reduce_fx="cat")
+        self.add_state("weights", default=[], dist_reduce_fx="cat")
 
     def update(
         self,
@@ -43,7 +44,11 @@ class EdgeMetric(torchmetrics.Metric):
                 predicted = edges_predicted[mask].flatten()
                 target = edges_target[mask].flatten()
 
-                self.metrics.append(self.metric(predicted, target)[1])
+                current_metric = self.metric(predicted, target)[1]
+                if current_metric != current_metric:
+                    current_metric = torch.zeros(1, device=predicted.device)[0]
+
+                self.metrics.append(current_metric)
 
                 self.weights.append(count / pow(index * block_size, self.weight_power))
 