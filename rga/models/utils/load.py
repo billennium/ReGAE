d3707ed0af4c010e49e454ac83a68b538fd1f1f9
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2022-01-12 23:35:37 +0100

    add graph generation and evaluation script; fix metrics

--- rga/models/utils/load.py
+++ rga/models/utils/load.py
@@ -1,16 +1,16 @@
 import yaml
 
 
-def load_hparams(params_path):
-    with open(params_path, "r") as stream:
+def load_hparams(hparams_path):
+    with open(hparams_path, "r") as stream:
         try:
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
 
-def load_model(params_path, model_path, model_class):
+def load_model(hparams_path, model_path, model_class):
     model = model_class.load_from_checkpoint(
-        checkpoint_path=model_path, hparams_file=params_path
+        checkpoint_path=model_path, hparams_file=hparams_path
     )
     return model
