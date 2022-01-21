544dac70ce7122d9f618e80e2705cffad32acc46
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2022-01-21 16:44:25 +0100

    add matrix evaluation script

--- rga/util/sparse.py
+++ rga/util/sparse.py
@@ -1,14 +1,22 @@
+from attr import has
 import torch
+import numpy as np
 from torch import Tensor
 
 
 def to_sparse_if_not(t: Tensor) -> Tensor:
+    if isinstance(t, (np.ndarray, np.generic)):
+        return t
     if t.layout == torch.strided:
         return t.to_sparse()
     return t
 
 
 def to_dense_if_not(t: Tensor) -> Tensor:
+    if isinstance(t, (np.ndarray, np.generic)):
+        return t
     if t.layout == torch.sparse_coo:
+        if not t.is_cuda and t.dtype == torch.float16:
+            t = t.float()
         return t.to_dense()
     return t
