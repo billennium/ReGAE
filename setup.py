a0ec4219a13e6f5814e2e7d95005690c0c58c3e0
Adam Małkowski <adam.malkowski@billennium.com>
2022-06-23 15:52:52 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ setup(
     description="R-GAE: Graph autoencoder based on recursive neural networks",
     packages=find_packages(),
     install_requires=[
-        "tqdm>=4.62.1",
-        "pandas>=1.3.3",
-        "torchmetrics>=0.6.1",
-        "scipy>=1.7.1",
+        "tqdm==4.62.1",
+        "pandas==1.3.3",
+        "torchmetrics==0.6.1",
+        "matplotlib==3.4.3",
+        "scipy==1.7.1",
         "networkx==2.6.2",
         "torch==1.9.0",
-        "numpy>=1.21.2",
-        "pytorch_lightning>=1.5.6",
-        "tensorflow>=2.7.0",
+        "numpy==1.21.2",
+        "pytorch_lightning==1.5.6",
+        "setuptools==59.5.0"
     ],
 )
